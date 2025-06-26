8b2b18de635edda9c0e10523f3704a89547056d9
swetha1916 <swetha.anantha@gmail.com>
2025-06-26 21:42:13 +0400

    Added my name and a your boards text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import ContactBar from './Components/ContactBar';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="app-header">
+        <h1>Swetha Anantha Krishnan</h1>
+        <p className="subtitle">Your boards</p>
+      </header>
       <ContactBar />
       {/* Other components like Navbar, About, etc. */}
     </div>
