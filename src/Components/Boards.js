60335f3c1f672ab60ce40f8ad9b3e24d95157d0e
swetha1916 <swetha.anantha@gmail.com>
2025-06-28 11:02:30 +0400

    Added boards properly

--- src/Components/Boards.js
+++ src/Components/Boards.js
@@ -6,20 +6,21 @@ const boards = [
   { title: 'Projects'},
   { title: 'Contact'},
   { title: 'Hobbies'},
+  { title: 'Education' }
 ];
 
 const Boards = () => {
   return (
     <div className="boards-container">
-      {boards.map((board, index) => (
-        <div
-          key={index}
-          className="board-card"
-          style={{ backgroundColor: "#dcdcdc"}}
-        >
-          <h2>{board.title}</h2>
-        </div>
-      ))}
+        {boards.map((board, index) => (
+            <div key={index} className="board-wrapper">
+                <div
+                    className="board-card"
+                    style={{ backgroundColor: "#dfdfd8" }}
+                />
+                <h4 className="board-title">{board.title}</h4>
+            </div>
+        ))}
     </div>
   );
 };
