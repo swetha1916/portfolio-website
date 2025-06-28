808a7d4c794b642feecdac336047afb609231a8b
swetha1916 <swetha.anantha@gmail.com>
2025-06-28 15:55:03 +0400

    Added a few pictures for some boards

--- src/Components/Boards.js
+++ src/Components/Boards.js
@@ -1,36 +1,43 @@
 import React from 'react';
 import './Boards.css';
+import Violin from '../Assets/Violin.jpg';
+import Painting from '../Assets/Painting.jpg';
+import Travel from '../Assets/Travel.jpg';
+import Coding from '../Assets/Coding.jpg'
+import Code from '../Assets/Code.jpg'
+import Mac from '../Assets/Mac.jpg'
+import Python from '../Assets/Python.jpg'
+import SQL from '../Assets/SQL.jpg'
+import Html from '../Assets/Html.jpg'
 
 const boards = [
   { title: 'About Me',
     pins: '39829',
-    images: [ 'https://unsplash.it/400/400?image=1025',
-              'https://unsplash.it/200/200?image=1005',
-              'https://unsplash.it/200/200?image=1031'
-            ]
+    images: [ Mac, Coding, Code ]
   },
-  { title: 'Projects',
-    pins: '12',
-    images: [ 'https://unsplash.it/400/400?image=1025',
-              'https://unsplash.it/200/200?image=1005',
-              'https://unsplash.it/200/200?image=1031'
-            ]
+  { title: 'Skills',
+    pins: '55',
+    images: [ SQL, Python, Html ]
   },
-  { title: 'Contact',
+  { title: 'Hobbies',
+    pins: '1092',
+    images: [ Violin, Painting, Travel ]
+  },
+  { title: 'Clubs and Roles',
     pins: '5',
     images: [ 'https://unsplash.it/400/400?image=1025',
               'https://unsplash.it/200/200?image=1005',
               'https://unsplash.it/200/200?image=1031'
             ]
   },
-  { title: 'Hobbies',
-    pins: '1092',
+  { title: 'Projects',
+    pins: '20',
     images: [ 'https://unsplash.it/400/400?image=1025',
               'https://unsplash.it/200/200?image=1005',
               'https://unsplash.it/200/200?image=1031'
             ]
   },
-  { title: 'Education',
+  { title: 'My Contact',
     pins: '20',
     images: [ 'https://unsplash.it/400/400?image=1025',
               'https://unsplash.it/200/200?image=1005',
