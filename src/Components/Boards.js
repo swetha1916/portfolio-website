476fec28498dbf7fe1c17c74efbd7e5a2a64c22f
swetha1916 <swetha.anantha@gmail.com>
2025-06-28 14:33:34 +0400

    Added 3 images per board

--- src/Components/Boards.js
+++ src/Components/Boards.js
@@ -2,25 +2,66 @@ import React from 'react';
 import './Boards.css';
 
 const boards = [
-  { title: 'About Me'},
-  { title: 'Projects'},
-  { title: 'Contact'},
-  { title: 'Hobbies'},
+  { title: 'About Me',
+    images: [ 'https://unsplash.it/400/400?image=1025',
+              'https://unsplash.it/200/200?image=1005',
+              'https://unsplash.it/200/200?image=1031'
+            ]
+  },
+  { title: 'Projects',
+    images: [ 'https://unsplash.it/400/400?image=1025',
+              'https://unsplash.it/200/200?image=1005',
+              'https://unsplash.it/200/200?image=1031'
+            ]
+  },
+  { title: 'Contact',
+    images: [ 'https://unsplash.it/400/400?image=1025',
+              'https://unsplash.it/200/200?image=1005',
+              'https://unsplash.it/200/200?image=1031'
+            ]
+  },
+  { title: 'Hobbies',
+    images: [ 'https://unsplash.it/400/400?image=1025',
+              'https://unsplash.it/200/200?image=1005',
+              'https://unsplash.it/200/200?image=1031'
+            ]
+  },
+  { title: 'Contact',
+    images: [ 'https://unsplash.it/400/400?image=1025',
+              'https://unsplash.it/200/200?image=1005',
+              'https://unsplash.it/200/200?image=1031'
+            ]
+  },
+  { title: 'Hobbies',
+    images: [ 'https://unsplash.it/400/400?image=1025',
+              'https://unsplash.it/200/200?image=1005',
+              'https://unsplash.it/200/200?image=1031'
+            ]
+  }
+  /* ,
   { title: 'Education' }
+   */
 ];
 
 const Boards = () => {
   return (
     <div className="boards-container">
-        {boards.map((board, index) => (
-            <div key={index} className="board-wrapper">
-                <div
-                    className="board-card"
-                    style={{ backgroundColor: "#dfdfd8" }}
-                />
-                <h4 className="board-title">{board.title}</h4>
+      {boards.map((board, index) => (
+        <div key={index} className="board-wrapper">
+          <div className="board-card">
+            <div className="board-preview-layout">
+              <div className="left-preview">
+                <img src={board.images[0]} alt="preview 1" />
+              </div>
+              <div className="right-preview">
+                <img src={board.images[1]} alt="preview 2" />
+                <img src={board.images[2]} alt="preview 3" />
+              </div>
             </div>
-        ))}
+          </div>
+          <h4 className="board-title">{board.title}</h4>
+        </div>
+      ))}
     </div>
   );
 };
