30d73ff838162ce16ae7266c036f26dd395da310
swetha1916 <swetha.anantha@gmail.com>
2025-06-29 19:14:34 +0400

    Added a logo and changed a few aspects of the contact bar

--- src/Components/ContactBar.js
+++ src/Components/ContactBar.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './ContactBar.css';
+import Slogo from '../Assets/Slogo.png';
 
 const ContactBar = () => {
   return (
     // to associate each icon with a link 
     <div className="contact-bar">
-      <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" title="GitHub">
+
+      <a href="/" title="Home">
+        <img src={Slogo} alt="Logo" className="contact-logo" />
+      </a>
+
+      <a href="https://github.com/swetha1916" target="_blank" rel="noopener noreferrer" title="GitHub">
         <i className="fab fa-github"></i>
       </a>
-      <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" title="LinkedIn">
+
+      <a href="https://linkedin.com/in/swetha-a-krishnan" target="_blank" rel="noopener noreferrer" title="LinkedIn">
         <i className="fab fa-linkedin-in"></i>
       </a>
-      <a href="mailto:yourgmail@gmail.com" title="Gmail">
+
+      <a href="mailto:swetha.ananthak@gmail.com" title="Gmail">
         <i className="fas fa-envelope"></i>
       </a>
+
     </div>
   );
 };
