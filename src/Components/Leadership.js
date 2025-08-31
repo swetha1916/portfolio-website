b31da395ec64fa8c7dc6968ab9a29892e8acdd51
swetha1916 <swetha.anantha@gmail.com>
2025-08-31 17:14:24 -0400

    Finished leadership experience board except for bug

--- src/Components/Leadership.js
+++ src/Components/Leadership.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import './Leadership.css';
 import Masonry from 'react-masonry-css';
 
-import SISA from '../Assets/Leadership board/SISA.png';
-import StartCoding from '../Assets/Leadership board/StartCoding.jpg';
-import PinkInTech from '../Assets/Leadership board/PinkInTech.jpg';
+import SISA from '../Assets/Leadership board/SISA.jpeg';
+import StartCoding from '../Assets/Leadership board/StartCoding.jpeg';
+import PinkInTech from '../Assets/Leadership board/PinkInTech.jpeg';
 import CSS from '../Assets/Leadership board/CSS.jpg';
-import Robotics from '../Assets/Leadership board/Robotics.jpg';
+import Robotics from '../Assets/Leadership board/Robotics.png';
 
 const leadershipStack = [
-    { name: 'McMaster Computer Science Society', image: CSS, type: "Club" },
-    { name: 'McMaster Start Coding', image: StartCoding, type: "Club" },
-    { name: 'McMaster South Indian Students Association', image: SISA, type: "Club" },
     { name: 'McMaster PinkInTech', image: PinkInTech, type: "Club" },
-    { name: 'World Robotics Olympiad', image: Robotics, type: "Contest" }
+    { name: 'McMaster Start Coding', image: StartCoding, type: "Club" },
+    { name: 'World Robotics Olympiad', image: Robotics, type: "Contest" },
+    { name: 'McMaster Computer Science Society', image: CSS, type: "Club" },
+    { name: 'McMaster South Indian Students Association', image: SISA, type: "Club" },
   // add more
 ];
 
@@ -64,7 +64,32 @@ const Leadership = () => {
       {isTldrActive ? (
         <div className="leadership-summary">
           <p className="leadership-text">
-          Hellooss
+            <b>Coding Mentor - McMaster Start Coding </b> <br/> 
+            • Taught programming to 4th-8th graders in St. Luke Catholic Elementary School, Sandhills Public School and Pauline Johnson Elementary School. <br/>
+            • Introduced students to the fundamentals of coding and functional programming concepts.<br/>
+            • Assisted with debugging, answered questions, and helped make programming more engaging. <br/>
+            <br/> <br/> 
+            
+            <b>Event Programming and Partnerships Lead - McMaster Computer Science Society</b> <br/> 
+            • Previously a year representative where I communicated the concerns of students in my year and helped in planning academic help sessions.<br/>
+            • Designed the structure and flow of events, making sure they were impactful and meaningful to the Computer Science community.<br/>
+            • Communicated with event guests, partners, sponsors, and other clubs regarding event details. <br/>
+            <br/> <br/> 
+
+            <b>Technical Team Member - McMaster PinkInTech</b> <br/> 
+            • Was the lead speaker at a lecture where I delivered an in-depth presentation about version control and how to use Git and GitHub, including live demos. <br/>
+            • Assisted in planning help sessions and other tech-related events aimed to empower women and other minorities in tech. <br/>
+            <br/> <br/> 
+
+            <b>Director of Events - McMaster South Indian Students Association </b> <br/> 
+            • Led the planning and execution of cultural south indian events like Onam, Diwali, Pongal, and Ugadi.<br/>
+            • Designed interactive activities to engage the audience and coordinated cross-club collaborations.<br/>
+            • Ensured the events celebrated diversity by making it inclusive to all types of students through events like game nights, karaoke nights, bonfires and murder mysteries.<br/>
+            <br/> <br/> 
+
+            <b>World Robotics Olympiad </b> <br/> 
+            • Led a team of 3 in building and programming a color-sensing and obstacle avoiding autonomous robot from scratch using Lego EV3 Mindstorm.<br/>
+            <br/> <br/> 
           </p>
         </div>
       ) : (
